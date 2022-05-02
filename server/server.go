e94849accc30fdb139f6c9cdb155404b3c3522f5
Ask Bjørn Hansen <ask@develooper.com>
2022-05-01 20:28:34 -0700

    api server: move tls validation functions

--- server/server.go
+++ server/server.go
@@ -3,12 +3,9 @@ package server
 import (
 	"context"
 	"crypto/tls"
-	"crypto/x509"
 	"database/sql"
-	"errors"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -17,7 +14,6 @@ import (
 	"go.ntppool.org/monitor/api/pb"
 	apitls "go.ntppool.org/monitor/api/tls"
 	"go.ntppool.org/monitor/ntpdb"
-	sctx "go.ntppool.org/monitor/server/context"
 	"go.ntppool.org/monitor/server/metrics"
 	twirpmetrics "go.ntppool.org/monitor/server/metrics/twirp"
 	"go.ntppool.org/monitor/server/vault"
@@ -54,29 +50,6 @@ func NewServer(cfg Config, dbconn *sql.DB) (*Server, error) {
 		tokens: tm,
 		m:      metrics,
 	}, nil
-}
-
-func (srv *Server) getVerifiedCert(verifiedChains [][]*x509.Certificate) (*x509.Certificate, string) {
-	for _, chain := range verifiedChains {
-		for _, cert := range chain {
-			for _, name := range cert.DNSNames {
-				// log.Printf("verified cert, dns name: %+v", cert.DNSNames)
-				// log.Printf("issuer: %s", cert.Issuer)
-				if strings.HasSuffix(name, ".mon.ntppool.dev") {
-					return cert, name
-				}
-			}
-		}
-	}
-	return nil, ""
-}
-
-func (srv *Server) verifyClient(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
-	cert, _ := srv.getVerifiedCert(verifiedChains)
-	if cert != nil {
-		return nil
-	}
-	return errors.New("no valid certificate found")
 }
 
 func (srv *Server) Run() error {
@@ -136,17 +109,6 @@ func (srv *Server) Run() error {
 	}
 
 	return server.ListenAndServeTLS("", "")
-}
-
-func (srv *Server) certificateMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		_, name := srv.getVerifiedCert(r.TLS.VerifiedChains)
-
-		ctx := context.WithValue(r.Context(), sctx.CertificateKey, name)
-		rctx := r.WithContext(ctx)
-		next.ServeHTTP(w, rctx)
-	})
 }
 
 func NewLoggingServerHooks() *twirp.ServerHooks {
---
+++ server/tls.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"context"
+	"crypto/x509"
+	"errors"
+	"net/http"
+	"strings"
+
+	sctx "go.ntppool.org/monitor/server/context"
+)
+
+func (srv *Server) certificateMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		_, name := srv.getVerifiedCert(r.TLS.VerifiedChains)
+
+		ctx := context.WithValue(r.Context(), sctx.CertificateKey, name)
+		rctx := r.WithContext(ctx)
+		next.ServeHTTP(w, rctx)
+	})
+}
+
+func (srv *Server) getVerifiedCert(verifiedChains [][]*x509.Certificate) (*x509.Certificate, string) {
+	for _, chain := range verifiedChains {
+		for _, cert := range chain {
+			for _, name := range cert.DNSNames {
+				// log.Printf("verified cert, dns name: %+v", cert.DNSNames)
+				// log.Printf("issuer: %s", cert.Issuer)
+				if strings.HasSuffix(name, ".mon.ntppool.dev") {
+					return cert, name
+				}
+			}
+		}
+	}
+	return nil, ""
+}
+
+func (srv *Server) verifyClient(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+	cert, _ := srv.getVerifiedCert(verifiedChains)
+	if cert != nil {
+		return nil
+	}
+	return errors.New("no valid certificate found")
+}
