72b7b9ec3ce88458ff6e90a23917148fab765b5d
Ask Bjørn Hansen <ask@develooper.com>
2022-12-14 18:35:46 -0800

    client: don't add timestamps to log output when running under systemd

--- client/cmd/cmd.go
+++ client/cmd/cmd.go
@@ -1,11 +1,19 @@
 package cmd
 
 import (
+	"log"
+	"os"
+
 	"github.com/spf13/cobra"
 	"go.ntppool.org/monitor/version"
 )
 
 func (cli *CLI) RootCmd() *cobra.Command {
+
+	if len(os.Getenv("INVOCATION_ID")) > 0 {
+		// don't add timestamps when running under systemd
+		log.Default().SetFlags(0)
+	}
 
 	cmd := &cobra.Command{
 		Use:   "ntppool-monitor",
